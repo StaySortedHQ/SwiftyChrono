4987ee1c65c1838a78471afe0b41f80920205d01
Leo Tumwattana <leo.tumwattana@gmail.com>
2025-07-19 19:48:00 +0800

    Make Language CaseIterable

--- Sources/SwiftyChrono/Language.swift
+++ Sources/SwiftyChrono/Language.swift
@@ -5,8 +5,12 @@
 //  Created by Changwat Tumwattana on 19/7/2025.
 //
 
-public enum Language {
+public enum Language: CaseIterable {
     
+    // -------------
+    // MARK: - Cases
+    // -------------
+
     case english
     case spanish
     case french
